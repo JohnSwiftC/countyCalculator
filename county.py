d4a6ec0b539805da7faa60efcbbd372014123fb3
Leo Klemm <lklemm@outlook.com>
2025-09-01 19:54:36 -0400

    some pd stuff

--- county.py
+++ county.py
@@ -1,1 +1,19 @@
-x
+import pandas as pd
+
+class CountyCalc:
+    def __init__(self, csv: str):
+        data_frame = pd.read_csv(csv)
+        self.data = data_frame
+        self.data.columns = [column.replace(" ", "_") for column in self.data.columns]
+        self.data.columns = [column.replace(",", "") for column in self.data.columns]
+    
+    def get_county_info(self, county: str):
+        result = self.data.query('name == ' + county, inplace=False)
+        return result
+
+
+calculator = CountyCalc("data-xQ5ws.csv")
+
+row = calculator.get_county_info("Polk_County_Iowa")
+
+print(row)
