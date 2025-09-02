8f39faea9b75058f29fb08081b7bc213c74f9984
Leo Klemm <lklemm@outlook.com>
2025-09-01 20:04:46 -0400

    Select county by name from data set

--- county.py
+++ county.py
@@ -4,16 +4,15 @@ class CountyCalc:
     def __init__(self, csv: str):
         data_frame = pd.read_csv(csv)
         self.data = data_frame
-        self.data.columns = [column.replace(" ", "_") for column in self.data.columns]
-        self.data.columns = [column.replace(",", "") for column in self.data.columns]
     
     def get_county_info(self, county: str):
-        result = self.data.query('name == ' + county, inplace=False)
-        return result
+        return self.data.loc[self.data['name'] == county]
+    
+    def show_data(self):
+        print(self.data)
 
 
 calculator = CountyCalc("data-xQ5ws.csv")
 
-row = calculator.get_county_info("Polk_County_Iowa")
+print(calculator.get_county_info("Polk County, Iowa")['effective_prop_tax_rate_23'])
 
-print(row)
