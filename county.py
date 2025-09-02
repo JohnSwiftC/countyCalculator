d39a962b00c805c619f9270877b77d6a3beda89d
Leo Klemm <lklemm@outlook.com>
2025-09-01 20:18:55 -0400

    cost for county function in countycalc class

--- county.py
+++ county.py
@@ -8,11 +8,21 @@ class CountyCalc:
     def get_county_info(self, county: str):
         return self.data.loc[self.data['name'] == county]
     
+    def get_county_rate(self, county: str, pretty=False):
+        val = self.get_county_info(county)['effective_prop_tax_rate_23']
+
+        if pretty:
+            val = val * 100
+
+        return val
+    
+    def get_cost_for_county(self, taxable_amount, county):
+        return self.get_county_rate(county) * taxable_amount
+    
     def show_data(self):
         print(self.data)
 
 
 calculator = CountyCalc("data-xQ5ws.csv")
 
-print(calculator.get_county_info("Polk County, Iowa")['effective_prop_tax_rate_23'])
-
+print(calculator.get_cost_for_county(100000, "Polk County, Iowa"))
